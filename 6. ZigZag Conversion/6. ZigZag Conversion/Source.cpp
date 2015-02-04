a980063edf5c4a59b882835e825388a5fbd12591
Shin <albusshin@gmail.com>
2015-02-05 00:58:59 +0800

    6.1

---
+++ 6. ZigZag Conversion/6. ZigZag Conversion/Source.cpp
@@ -0,0 +1,51 @@
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    string convert(string s, int nRows) {
+		if (s.size() == 0)
+		{
+			return s;
+		}
+		vector<vector<char>> zz;
+		zz.resize(nRows);
+		string ret = "";
+		int i=0, k=0;
+		bool down = true;
+		zz[0].push_back(s[0]);
+		for (k=1; k<s.size(); k++)
+		{
+			if (down)
+			{
+				i++;
+				zz[i].push_back(s[k]);
+				if (i == nRows) down = false;
+			}
+			else
+			{
+				i--;
+				zz[i].push_back(s[k]);
+				if (i == 0) down = true;
+			}
+		}
+		stringstream sstr;
+		for (vector<vector<char>>::iterator ite = zz.begin(); ite!=zz.end(); ite++)
+		{
+			sstr << string(*ite->begin(), *ite->end());
+		}
+		sstr >> ret;
+		return ret;
+    }
+};
+
+void main()
+{
+	Solution ss;
+	string res = ss.convert("PAYPALISHIRING", 3);
+	cout << res;
+}
