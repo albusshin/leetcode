cef98a4aa66823f2096db914e9f637576981f115
Shin <albusshin@gmail.com>
2015-02-10 20:54:52 +0800

    8.2

--- 8. String to Integer (atoi)/8. String to Integer (atoi)/Source.cpp
+++ 8. String to Integer (atoi)/8. String to Integer (atoi)/Source.cpp
@@ -4,12 +4,24 @@
 class Solution {
 private:
 	char *p;
-	int pow10(int digit) {
-		int res = 1;
+	char digits[10];
+	long long pow10(int digit) {
+		long long res = 1;
 		for (int i=0; i<digit; i++) res *= 10;
 		return res;
 	}
+	int isDigit(char c) {
+		for (int i=0; i<10; i++) {
+			if (c == digits[i]) return true;
+		}
+		return false;
+	}
 public:
+	Solution() {
+		for (int i=0; i<10; i++) {
+			digits[i] = i + '0';
+		}
+	}
     int atoi(const char *str) {
 		p = (char*) str;
 		while (isspace(*p)) p++;
@@ -17,21 +29,34 @@ public:
 		if (*p == '+') {
 			if(*(p+1) == '-') return 0;
 			p++;
-		};
+		}
 		if (*p == '-') {
-			if ((*p+1) == '+') return 0;
+			if (*(p+1) == '+') return 0;
 			p++;
 			negative = true;
 		}
-		int digits = strlen(p) - 1;
-		int res = 0;
-		while (*p != NULL) {
+		char *num = new char[strlen(str)];
+		int i = 0;
+		char *pTmp = p;
+		while (*pTmp != NULL && isDigit(*pTmp)) {
+			num[i++] = *pTmp;
+			pTmp++;
+		}
+		num[i] = 0;
+		int digits = strlen(num) - 1;
+		if (digits > 9) {
+			if (!negative) return 2147483647;
+			else return -2147483648;
+		}
+		long long res = 0, tmpRes;
+		while (*p != NULL && isDigit(*p)) {
 			res += (*p - '0') * pow10(digits--);
+			tmpRes = negative ? -res : res;
+			if (tmpRes > 2147483647) return 2147483647;
+			else if (tmpRes < -2147483648) return -2147483648;
 			p++;
 		}
 		if (negative) res = -res;
-		if (res < 0 && !negative) return -2147483648;
-		else if (res > 0 && negative) return 2147483647;
-		else return res;
+		return (int)res;
     }
 };
