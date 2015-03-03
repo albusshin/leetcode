f2320d922e842b79193c95c498bc35650f77aafa
Shin <albusshin@gmail.com>
2015-03-03 16:04:53 +0800

    23.2 Using map

--- 23. Merge k Sorted Lists/23. Merge k Sorted Lists/Source.cpp
+++ 23. Merge k Sorted Lists/23. Merge k Sorted Lists/Source.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <map>
 
 using namespace std;
 
@@ -11,54 +12,28 @@ struct ListNode {
 };
 
 class Solution {
+	multimap<int, int> mp;
 public:
     ListNode *mergeKLists(vector<ListNode *> &lists) {
 		ListNode *head = new ListNode(0), *p = head;
-		bool empty = true;
-		for (auto l : lists) {
-			if (l != nullptr) {
-				empty = false;
-				break;
-			}
+
+		int lsize = lists.size();
+		for (int i = 0; i < lsize; i++) {
+			auto l = lists[i];
+			if(l != nullptr) mp.insert(make_pair(l->val, i));
 		}
-		while (!empty) {
-			auto ite = min_element(lists.begin(), lists.end(), [] (const ListNode* l, const ListNode* r) {
-				bool ret = false;
-				if (l != nullptr && r != nullptr) ret = l->val < r->val;
-				else if (l == nullptr) ret = false;
-				else if (r == nullptr) ret = true;
-				return ret;
-			});
-			p->next = *ite;
+
+		while (!mp.empty()) {
+			p -> next = new ListNode(mp.begin()->first);
 			p = p->next;
-			(*ite) = (*ite)->next;
-			empty = true;
-			for (auto l : lists) {
-				if (l != nullptr) {
-					empty = false;
-					break;
-				}
-			}
+			int i = mp.begin()->second;
+			lists[i] = lists[i]->next;
+			mp.erase(mp.begin());
+			if (lists[i] != nullptr) mp.insert(make_pair(lists[i]->val, i));
 		}
+
 		ListNode* ret = head->next;
 		delete head;
 		return ret;
     }
 };
-
-int main() {
-	Solution s;
-	vector<ListNode *> lists;
-	ListNode *p1 = new ListNode(1);
-	p1->next = new ListNode(2);
-	p1->next->next = new ListNode(2);
-	ListNode *p2 = new ListNode(1);
-	p2->next = new ListNode(1);
-	p2->next->next = new ListNode(2);
-	lists.push_back(p1);
-	lists.push_back(p2);
-	auto l = s.mergeKLists(lists);
-	for (ListNode* p = l; p != nullptr; p = p->next) {
-		cout << p->val;
-	}
-}
