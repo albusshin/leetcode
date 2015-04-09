8480fed9e59c94eb1830cb64f37fe1d5f1a778b2
Shin <albusshin@gmail.com>
2015-04-09 14:52:04 +0800

    136.2 using xor

--- 136. Single Number/136. Single Number/Source.cpp
+++ 136. Single Number/136. Single Number/Source.cpp
@@ -1,19 +1,8 @@
-#include <set>
-
-using namespace std;
-
 class Solution {
 public:
     int singleNumber(int A[], int n) {
-		set<int> s;
-		for (int i=0; i<n; i++)
-		{
-			int e = A[i];
-			if (s.find(e) == s.end())
-				s.insert(e);
-			else
-				s.erase(e);
-		}
-		return *(s.begin());
+        int ret = 0;
+        for (int i = 0; i < n; i++) ret ^= A[i];
+        return ret;
     }
 };
