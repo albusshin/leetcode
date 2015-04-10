4156a6185437a0ab96b9548ec68ff8bca8cf082d
Shin <albusshin@gmail.com>
2015-04-10 21:50:43 +0800

    179

---
+++ 179. Largest Number/179. Largest Number/Source.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string largestNumber(vector<int> &num) {
+		sort(num.begin(), num.end(), [&](int num1, int num2){
+			stringstream tmpss;
+			string str1, str2;
+			tmpss << num1 << num2;
+			tmpss >> str1;
+			tmpss.clear();
+			tmpss << num2 << num1;
+			tmpss >> str2;
+			return str1 < str2;
+		});
+
+		for (auto i : num) cout << i << " ";
+		cout << endl;
+		stringstream ss;
+		for (auto rite = num.rbegin(); rite != num.rend(); ++rite) {
+			ss << (*rite);
+		}
+		string ret;
+		ss >> ret;
+		bool all0 = true;
+		for (auto c : ret)
+			if (c != '0')
+				all0 = false;
+		if (all0) return "0";
+		return ret;
+    }
+};
