cc68fab97fc83a857957c238b9bd702bf369caa3
Shin <albusshin@gmail.com>
2015-05-03 23:50:33 +0800

    162

---
+++ 162 Find Peak Element/162 Find Peak Element/Source.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        if (nums.size() == 1) return 0;
+        for(int i = 1; i < nums.size() - 1; i++) {
+            if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) return i;
+        }
+        return nums[0] > nums[nums.size() - 1] ? 0 : nums.size() - 1;
+    }
+};
