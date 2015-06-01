d3be2d21a9002429744b927f5e9fdbc55723fca4
Shin <albusshin@gmail.com>
2015-06-01 21:58:32 +0800

    215, using sort, not cool

---
+++ 215 Kth Largest Element in an Array/215 Kth Largest Element in an Array/Source.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end(), [](int a, int b){return a > b;});
+        return nums[k - 1];
+    }
+};
